65aff6ec5daea2ceaad5ecee60adaaedf2d21a78
James Declerus <james.declerus@esih.edu>
2019-04-08 19:04:14 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,8 +35,8 @@ def turn(board)
 end
 
 #CHECK FOR EMPTY SPACE
-def position_taken?(board, index)
-  !
+def !position_taken?(board, index)
+
   if board[index]== "X" || board[index] == "O"
     return true
   else board[index] == " " ||  board[index] == " " ||  board[index]== nil
