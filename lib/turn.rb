d67b3ba0bfadabc27b817c6c1e9f9d51d5decc51
James Declerus <james.declerus@esih.edu>
2019-04-08 18:59:36 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -39,7 +39,7 @@ def position_taken?(board, index)
   !
   if board[index]=="X" || board[index] == "O"
     return true
-  else (board[index] == " "|| board[index]== nil)
+  else board[index] == " "|| board[index]== nil
     return false
   end
 end
