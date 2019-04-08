daaeb8bdd2d48ae5762473dace4f3e513041095f
James Declerus <james.declerus@esih.edu>
2019-04-08 18:58:22 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -46,7 +46,7 @@ end
 
 #IS # CORRECT & SPACE EMPTY?
 def valid_move?(board,index)
-	if (!position_taken?(board, index) && index.between?(0,8))
+	if !position_taken?(board, index) && index.between?(0,8)
 		return true
  	else
 		return false
